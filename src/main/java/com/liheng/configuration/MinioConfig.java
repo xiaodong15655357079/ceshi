3be594aa47e4a90226ab49cfb9fa367ff808bff3
董继程 <11488341+dong-jicheng@user.noreply.gitee.com>
2023-01-15 20:50:32 +0800

    1

---
+++ src/main/java/com/liheng/configuration/MinioConfig.java
@@ -0,0 +1,27 @@
+package com.liheng.configuration;
+
+import io.minio.MinioClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+public class MinioConfig {
+
+    private String endpoint;
+    private String accessKey;
+    private String secretKey;
+
+    @Bean
+    public MinioClient getMinioClient(){
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+        return minioClient;
+    }
+
+}
---
+++ src/main/java/com/liheng/controller/MinioController.java
@@ -0,0 +1,53 @@
+package com.liheng.controller;
+
+
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+
+import io.minio.errors.*;
+import org.apache.http.entity.ContentType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("minio")
+public class MinioController {
+
+
+
+    @Autowired
+    private MinioClient client;
+
+
+    @GetMapping("/upload")
+    public String MinIOUpload(@RequestParam String  name) throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+        File file=new File("C:\\Users\\Hasee\\"+name+".mp4");
+
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        MultipartFile multipartFile = new MockMultipartFile(file.getName(), file.getName(),
+                ContentType.APPLICATION_OCTET_STREAM.toString(), fileInputStream);
+        //2. 文件上传  参数校验 和分片上传
+        PutObjectArgs putObjectArgs = PutObjectArgs.builder().object(name + ".mp4")
+                .bucket("xiaodongtest")
+                .stream(multipartFile.getInputStream(), multipartFile.getSize(), -1)
+                .contentType(multipartFile.getContentType())
+                .build();
+        client.putObject(putObjectArgs);
+        System.out.println("http://192.168.136.160:9000/xiaodongtest/"+name+".mp4");
+            return "成功";
+
+    }
+
+
+
+}
